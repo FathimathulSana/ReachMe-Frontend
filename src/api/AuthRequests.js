41fe732e3517c1da7a6a806a492ba8122376227f
FathimathulSana <fathimathusana21@gmail.com>
2023-02-24 14:17:24 +0530

    URL added

--- src/api/AuthRequests.js
+++ src/api/AuthRequests.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const API = axios.create({ baseURL: 'http://localhost:5050' });
+const API = axios.create({ baseURL: 'https://reachme.thesak.online' });
 
 export const logIn = (formData) => API.post('/auth/login', formData);
 
--- src/api/UserRequest.js
+++ src/api/UserRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const API = axios.create({ baseURL: 'http://localhost:5050' });
+const API = axios.create({ baseURL: 'https://reachme.thesak.online' });
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('adminProfile')) {
